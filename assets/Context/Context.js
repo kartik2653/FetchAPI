38f43c465e3ba134277ab9701b893e71f219bbcc
Kartik <kartikthakur2653@gmail.com>
2022-06-28 21:31:58 +0530

    Unnecessary files removed

--- assets/Context/Context.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-import React, {createContext} from 'react';
-
-const context= createContext();
-export const updatingContext=createContext();
-export default context;
